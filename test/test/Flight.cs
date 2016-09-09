de1794ede466e76ed2bcc28e4b305de66637261e
Zvone <Zvone@DESKTOP-G83L2N9>
2016-09-10 00:29:54 +0200

    Parsing

---
+++ test/test/Flight.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+	class Flight
+	{
+		public string Origin { get; set; }
+
+		public string Destination { get; set; }
+
+		public DateTime DepartureDate { get; set; }
+
+		public DateTime ReturnDate { get; set; }
+
+		public int AdultsNumber { get; set; }
+
+		public int ChildrenNumber { get; set; }
+
+		public int InfantsNumber { get; set; }
+
+		public decimal Currency { get; set; }
+	}
+}
